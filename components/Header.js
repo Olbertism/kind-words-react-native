f317ed9699865e5571331e4f20d59f4ca51f5033
Albert Pichler <albertpichler@gmx.net>
2022-06-17 12:05:22 +0200

    Add first state

---
+++ components/Header.js
@@ -0,0 +1,41 @@
+import { Pacifico_400Regular, useFonts } from '@expo-google-fonts/pacifico';
+import AppLoading from 'expo-app-loading';
+import Constants from 'expo-constants';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { colors } from '../styles/constants';
+
+export default function Header(props) {
+  let [fontsLoaded] = useFonts({
+    Pacifico_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <Text style={styles.label}>{props.label}</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: colors.cardBackground,
+  },
+  container: {
+    paddingTop: Constants.statusBarHeight + 10,
+    paddingBottom: 20,
+  },
+  label: {
+    color: colors.text,
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: 'Pacifico_400Regular',
+  },
+});
